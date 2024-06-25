3287a3c81e2957bcfcdd4bc2417e17bf285fb116
yaacovkdev <yaacov.kochatkov@gmail.com>
2024-06-25 15:49:06 -0700

    broken up styling for tablet and mobile

--- src/components/SubmitForm/SubmitForm.jsx
+++ src/components/SubmitForm/SubmitForm.jsx
@@ -1,18 +1,20 @@
 import { useEffect, useRef, useState } from "react";
 import "./SubmitForm.scss";
-import GenerateTable from "./GenerateTable";
+import GenerateTable from "../GenerateTable/GenerateTable";
+import StatusModal from "../StatusModal/StatusModal";
 
-function SubmitForm() {
+export default function SubmitForm() {
   const inputFormula = useRef();
   const [tableData, setTableData] = useState(null);
   const [errorMessage, setErrorMessage] = useState(false);
+  const [serverConnected, setServerConnected] = useState(false);
   const wss = useRef(null);
 
   useEffect(() => {
     wss.current = new WebSocket(process.env.REACT_APP_API_URL);
 
     wss.current.onopen = () => {
-      console.log("WebSocket connection to Calculator established");
+      setServerConnected(true);
     };
 
     wss.current.onmessage = (event) => {
@@ -46,26 +48,34 @@ function SubmitForm() {
   };
 
   return (
-    <div className="submit-form">
-      <div className="submit-form__input">
-        <div className="submit-form__input-field">
-          <input
-            id="boolean-input-field"
-            placeholder="Write Boolean Formula"
-            ref={inputFormula}
-          />
-          <button id="boolean-input-button" onClick={submitFormula}>
-            Solve
-          </button>
-        </div>
-        <p id="text-tag">Finally Here!</p>
-      </div>
+    <>
+      {serverConnected ? (
+        <div className="submit-form">
+          <div className="submit-form__input">
+            <div className="submit-form__input-field">
+              <input
+                id="boolean-input-field"
+                placeholder="Write Boolean Formula"
+                ref={inputFormula}
+              />
+              <button id="boolean-input-button" onClick={submitFormula}>
+                Solve
+              </button>
+            </div>
+            <p id="text-tag">Finally Here!</p>
+          </div>
 
-      <div className="submit-form__output">
-        {errorMessage?  <h3>{tableData}</h3> : <GenerateTable data={tableData} />}
-      </div>
-    </div>
+          <div className="submit-form__output">
+            {errorMessage ? (
+              <h3>{tableData}</h3>
+            ) : (
+              <GenerateTable data={tableData} />
+            )}
+          </div>
+        </div>
+      ) : (
+        <StatusModal />
+      )}
+    </>
   );
 }
-
-export default SubmitForm;
